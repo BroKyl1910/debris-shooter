3e861942a7ac8443d5ab229376500fb08618908f
Kyle <brooks.kyle621@gmail.com>
2022-12-11 07:28:55 +0200

    Moved code to separate files

--- main.js
+++ main.js
@@ -14,8 +14,10 @@ canvas.height = canvasConstraints.height;
 const ctx = canvas.getContext("2d");
 
 
+var gameContext = {};
 let player = new Player(canvasConstraints);
-let gun = new Gun(canvasConstraints, player.position);
+let gun = new Gun(canvasConstraints, player.position, gameContext);
+
 let gameObjects = [
     player,
     gun
@@ -29,25 +31,36 @@ for (let i = 0; i < Math.random() * 10; i++) {
 
 requestAnimationFrame(gameLoop);
 
+var paused = false;
+document.onkeydown = e => {
+    paused = !paused;
+}
+
 let lastElapsed = 0;
 function gameLoop(elapsed) {
     let delta = (elapsed - lastElapsed) / 10;
     lastElapsed = elapsed;
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.fillStyle = "rgb(51,51,51)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    gameObjects.filter(go => go.gameObjectType=='debris').forEach(d => {
-        if(d.position.y > canvasConstraints.height){
+    // control falling debris
+    gameObjects.filter(go => go.gameObjectType == 'debris').forEach(d => {
+        if (d.position.y > canvasConstraints.height) {
             gameObjects.splice(gameObjects.indexOf(d), 1);
             gameObjects.push(new Debris(canvasConstraints));
         }
     });
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "rgb(51,51,51)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    gameContext.delta = delta;
+    gameContext.gameObjects = gameObjects;
+
     gameObjects.forEach(go => {
-        go.update(delta);
+        if (!paused) {
+            go.update();
+        }
         go.draw(ctx);
     });
 
